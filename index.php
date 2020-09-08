d86259d0d7b0158b13cdda5de5c1b9afeedc01b7
ndanel <nath.dnl80@gmail.com>
2020-09-08 10:16:08 +0200

    envoie

--- index.php
+++ index.php
@@ -3,11 +3,20 @@
 <head>
     <meta charset="utf-8">
     <meta name="TPGPS" value="notranslate">
-    <meta name="referrer" content="strict-origin">
+    <link rel="icon" type="image/png" href="/favicon.png"/>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
     <link rel="stylesheet" href="/style.css">
     <title>Index</title>
 </head>
 <body>
+    <!--Header-->
+    <div>
+    </div>
+    <!--Corps-->
+    <div>
+    </div>
+    <!--Footer-->
+    <div>
+    </div>
 </body>
 </html>
