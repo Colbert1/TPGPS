aa1e4cd52bb657789e4375f3f68f572a998ca8e0
ndanel <nath.dnl80@gmail.com>
2020-09-15 10:15:17 +0200

    index

--- index.php
+++ index.php
@@ -4,9 +4,9 @@
     <meta charset="utf-8">
     <meta name="TPGPS" value="notranslate">
     <!--A revenir dessus-->
-    <link rel="icon" type="image/x-icon" href="favicon.ico/>
+    <link rel="icon" type="image/x-icon" href="favicon.ico"/>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
-    <link rel="stylesheet" href="/style.css">
+    <link rel="stylesheet" href="style.css">
     <title>Index</title>
 </head>
 <body>
@@ -14,7 +14,17 @@
     <div>
     </div>
     <!--Corps-->
-    <div>
+    <div id="container">
+        <!--Formulaire de connexion-->
+        <form action="verification.php" method="POST">
+            <h1>Identification</h1>
+
+            <p>Nom d'utilisateur :</p>
+            <input type="text" placeholder="Entrer le nom d'utilisateur" name="username" required>    
+            <p>Mot de passe :</p>
+            <input type="text" placeholder="Entrer le mot de passe" name="password" required>
+            <input type="submit" id="submit" value='LOGIN'>
+        </form>
     </div>
     <!--Footer-->
     <div>
